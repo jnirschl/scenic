a2b5a70b22e5493a05713e88a385de2b5dcad08f
Jeff Nirschl <jnirschl@stanford.edu>
2022-08-10 15:59:29 -0700

    Add slurm sbatch script

---
+++ slurm_job_gpu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+set -o pipefail
+
+#SBATCH --job-name=test_scenic_gpu
+#SBATCH --partition=pasteur --qos=normal
+#SBATCH --nodelist=pasteur1
+#SBATCH --output=test_scenic_%j.out # STDOUT logs %j=job id
+#SBATCH --error=./test_scenic_%j.err # Error logs %j=job id 
+#SBATCH --time=0:05:00 # Wall time limit days-HH:mm:ss
+#SBATCH --nodes=1 # Maximum number of nodes to be allocated
+#SBATCH --mem=4gb # Memory (e.g., RAM) in gb 
+#SBATCH --cpus-per-task=2 # Number of cores per task
+#SBATCH --gres=gpu:1
+
+
+# print information
+echo -e "SLURM_JOBID:\t$SLURM_JOBID
+SLURM_JOB_NODELIST:\t$SLURM_JOB_NODELIST
+SLURM_NNODES:\t$SLURM_NNODES
+SLURMTMPDIR:\t$SLURMTMPDIR
+Working directory:\t$SLURM_SUBMIT_DIR
+
+Allocating resources..."
+
+# sample process (list hostnames of the nodes you've requested)
+NPROCS=`srun --nodes=${SLURM_NNODES} bash -c 'hostname' |wc -l` 
+echo -e "NPROCS:\t$NPROCS"
+echo -e node_feat -p gpu | grep GPU_
+
+# list the allocated gpu, if desired
+#srun /usr/local/cuda/samples/1_Utilities/deviceQuery/deviceQuery
+
+echo -e "Resources are allocated (with GPU)."
+if [[ -f /sailhome/jnirschl/miniconda3/etc/profile.d/conda.sh ]]; then
+	echo -e "Activating conda environment scenic."
+	source /sailhome/jnirschl/miniconda3/etc/profile.d/conda.sh
+	conda activate scenic
+else
+	echo -e 'FileNotFoundError: /sailhome/jnirschl/miniconda3/etc/profile.d/conda.sh'
+exit 1
+fi
+
+## set env var
+export TF_CPP_MIN_LOG_LEVEL=0
+
+ ## run dvc
+echo -e "Running Scenic MNIST example with GPU..."
+cd "/sailhome/jnirschl/GitHub/scenic"
+python3 scenic/main.py --config=scenic/projects/baselines/configs/mnist/mnist_config.py --workdir=scenic/projects/mnist/
+
+echo -e "Exit code:	$?"
